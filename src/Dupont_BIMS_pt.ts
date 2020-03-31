528f611c5472b98623b32aadf0b429feefae0a6e
coffee247 <stallingsjm@mymail.vcu.edu>
2020-03-31 11:15:01 -0400

    Fixes

---
+++ src/Dupont_BIMS_pt.ts
@@ -0,0 +1,288 @@
+<?xml version="1.0"
+encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR" sourcelanguage="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="26"/>
+        <source>Dupont Ballistic Information Management System</source>
+        <translation>Sistema de Gestão de Informação Balística</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="223"/>
+        <source>Home</source>
+        <translation>Início</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="250"/>
+        <source>qrc:/HomePageText.html</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="301"/>
+        <source>History</source>
+        <translation>Histórico</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="369"/>
+        <source>PushButton</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="2175"/>
+        <source>cm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="449"/>
+        <source>Screen2 to Target</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="480"/>
+        <source>Screen1 to Screen 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="495"/>
+        <source>Muzzle to Middle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="505"/>
+        <source>Middle to Screen 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="574"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="614"/>
+        <source>Range Distances</source>
+        <translation>Medições de rango</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="642"/>
+        <source>Change these values in Settings page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="702"/>
+        <source>Grains</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="717"/>
+        <source>Projectile</source>
+        <translation>Projétil</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="794"/>
+        <source>Threat Variables</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="834"/>
+        <source>Powder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="859"/>
+        <source>Set Threat Variables in Settings Page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="960"/>
+        <source>  Fire shot  </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1013"/>
+        <source>Measure</source>
+        <translation>Medida</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1051"/>
+        <source>Project ID&apos;s</source>
+        <translation>Identidades de Projeto</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1071"/>
+        <source>Add new Project ID below:</source>
+        <translation>Adic. nova ID de Projeto abaixo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1885"/>
+        <source>Remove</source>
+        <translation>Remover</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="2019"/>
+        <source>Add new</source>
+        <translation>Adic. novo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1142"/>
+        <source>Hardware (Capture card) settings</source>
+        <translation>Configurações de Hardware (cartão de captura) </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1161"/>
+        <source>Screens Device and type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1171"/>
+        <source>Dev1/ctr0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1181"/>
+        <source>Dev1/ctr1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1191"/>
+        <source>120.0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1201"/>
+        <source>Magnet Device and type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1211"/>
+        <source>Default card timeout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1255"/>
+        <source>Balisticians</source>
+        <translation>Balistas</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1275"/>
+        <source>Add balistician below:</source>
+        <translation>“Adic. balista abaixo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1339"/>
+        <source>Edit Projectiles list</source>
+        <translation>Editar lista de projéteis</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1359"/>
+        <source>Add new projectile below:</source>
+        <translation>Adic. nova ID de Projeto abaixo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1385"/>
+        <source>Name or Type</source>
+        <translation>Nome ou Tipo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1398"/>
+        <source>Mass</source>
+        <translation>Massa</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1411"/>
+        <source>Drag Coef</source>
+        <translation>“Coef. De resist.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1634"/>
+        <source>Edit Powder list </source>
+        <translation>Editar lista de Pólvora</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1654"/>
+        <source>Add new powder below:</source>
+        <translation>“Adic. nova pólvora abaixo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1845"/>
+        <source>Edit Grain Weights list </source>
+        <translation>Editar lista de Grãos de Peso</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1865"/>
+        <source>Add new grains below:</source>
+        <translation>“Adic. novos grãos abaixo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="2106"/>
+        <source>Muzzle to Middle (muz/mid)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="2155"/>
+        <source>Screen2 to Target (s1/targ)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="2188"/>
+        <source>Save Range Values</source>
+        <translation>Salvar Val. de rango</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="2221"/>
+        <source>Screen1 to Screen 2 (s1/s2)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="2274"/>
+        <source>Middle to Screen 2 (mid/s2)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="2294"/>
+        <source>Range Measurements</source>
+        <translation>Medições de rango</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="2350"/>
+        <source>Settings</source>
+        <translation type="unfinished">Configurações</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="2425"/>
+        <source>Projects , Samples, and Fabrics</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="2472"/>
+        <source>  Home</source>
+        <translation>Início</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="2492"/>
+        <source>  History</source>
+        <translation>Histórico</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="2512"/>
+        <source>  Measure</source>
+        <translation>Medida</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="2532"/>
+        <source>  Settings</source>
+        <translation>Configurações</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="2549"/>
+        <source>Projects</source>
+        <translation>Projetos</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="2585"/>
+        <source>   Quit</source>
+        <translation>Sair</translation>
+    </message>
+</context>
+</TS>
